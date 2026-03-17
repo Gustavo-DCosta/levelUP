86e3529a170ef4e0819394e62ed67b44a9c0b2b7
Gustavo <gustagamerpereira@gmail.com>
2026-03-17 09:35:13 +0100

    ADD: "background picture"

--- app/src/main/java/com/example/levelup/MainActivity.kt
+++ app/src/main/java/com/example/levelup/MainActivity.kt
@@ -1,15 +1,20 @@
+
     package com.example.levelup
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.levelup.ui.theme.LevelUPTheme
 
@@ -20,6 +25,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             LevelUPTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(
+                                brush = Brush.linearGradient(
+                                    colors = listOf(
+                                        Color(0xFF5E9DCC),
+                                        Color(0xFF657CF4),
+                                        Color(0xFF2C4EFF)
+                                    )
+                                )
+                            )
+                    )
                     Greeting(
                         name = "Gustavo-Da Costa",
                         modifier = Modifier.padding(innerPadding)
